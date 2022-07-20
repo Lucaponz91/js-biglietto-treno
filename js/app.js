1dd18bf0117848afd72526284965f744df7ec7b2
Lucaponz91 <lucapo1991@msn.com>
2022-07-20 15:11:14 +0200

    add fullPrice

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@
 let totalKm = prompt('Quanti chilometri vuoi percorrere?')
 let age = prompt('Quanti anni hai?')
 console.log(totalKm, age)
+// Converto in parseInt
+totalKm = parseInt(totalKm)
+age = parseInt(age)
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let fullPrice = totalKm * 0.21;
+console.log(fullPrice)
+console.dir(full_price)
+full_price.innerHTML = fullPrice
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo : es 15.23 €).
